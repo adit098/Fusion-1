ee32180839a270c2144d6474e92333fd839d803d
adit098 <adityakumargupta151@gmail.com>
2021-03-21 12:41:04 +0530

    upload excel file first commit

--- hostel_management/views.py
+++ hostel_management/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from applications.globals.models import (Designation, ExtraInfo,
                                          HoldsDesignation, DepartmentInfo)
 from applications.academic_information.models import *
+import xlrd
 
 # Create your views here.
 
@@ -22,16 +23,24 @@ def view_alloted_room(request):
     return render(request, 'hostelmanagement/hostel.html', context)
 
 
-    def add_hall_room(request):
-    if request.method == "GET":
-        files = request.FILES['hallroom']
-        excel = pd.read_csv(files)
-        roll_no = excel['ROLL NO.']
-        hall_no = excel['Hall']
-        room_no = excel['ROOM NO']
-        for roll, hall, room in zip(roll_no,hall_no,room_no):
-            student = Student.objects.get(id__id=roll)
-            student.hall_no = hall
-            student.room_no = room
-            print("success GM==")
-    return render(request, 'hostelmanagement/hall_upload.html')
+def add_hall_room(request):
+    if request.method == "POST":
+        # print("inside")
+        files = request.FILES['hall4room']
+        excel = xlrd.open_workbook(file_contents=files.read())
+        hall_4_allotment = []
+        for sheet in excel.sheets():
+            for row in range(1, sheet.nrows):
+                roll_no = int(sheet.cell(row,1).value)
+                name = str(sheet.cell(row,2).value)
+                program = str(sheet.cell(row,4).value)
+                room_no = int(sheet.cell(row,7).value)
+                block = str(sheet.cell(row,8).value)
+                hall_4_allotment.append([roll_no, name, program, room_no, block])
+        # print(hall_4_allotment[:5])
+        hall_4_allotment = hall_4_allotment[:10]
+
+        context = {
+            'hall_4_allotment': hall_4_allotment
+        }
+    return render(request, 'hostelmanagement/hostel.html', context)
